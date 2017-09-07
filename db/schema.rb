be2dcc006589b442e11697ad207f23f881a85901
MrQile <quitalizner@gmail.com>
2017-09-07 06:01:03 -0700

    Add admin functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907114017) do
+ActiveRecord::Schema.define(version: 20170907124850) do
 
   create_table "product_types", force: :cascade do |t|
     t.string "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20170907114017) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
