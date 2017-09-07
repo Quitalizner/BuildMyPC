b88e6f06b2b77aecb3418ff79db61f53b1dd8042
MrQile <quitalizner@gmail.com>
2017-09-07 04:48:46 -0700

    Add bcrypt and password digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907112929) do
+ActiveRecord::Schema.define(version: 20170907114017) do
 
   create_table "product_types", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20170907112929) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
